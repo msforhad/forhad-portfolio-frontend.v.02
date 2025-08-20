2e14e82928d9fb5dff9411ad6743e3860b6e13f5
msforhad <msforhad717@gmail.com>
2025-08-21 03:34:18 +0600

    adding frontend code

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+import {ToastContainer} from 'react-toastify'
+import  { Route, Routes} from "react-router-dom";
+import Navbar from './components/UI/Navbar.jsx';
+import Footer from './components/UI/Footer.jsx';
+import Home from './pages/Home.jsx';
+import Skills from './pages/Skills.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import { useContext } from 'react';
+import { AppContext } from './context/AppContext.jsx';
+import ViewWorks from './pages/viewWorks.jsx';
+
+const App = ()=> {
+  const {mode}=useContext(AppContext)
+
+  return (
+    <div
+      className={`${
+        mode === "light" ? "light-mode" : "dark-mode"
+      } transition-colors duration-700 ease-in-out`}
+    >
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio/:name" element={<ViewWorks />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { myWorks } from "../assets/portfolioData.js";
+import { NavLink, useNavigate } from 'react-router-dom';
+
+const Portfolio = () => {
+  const navigate=useNavigate()
+
+  return (
+    <div className="bg-primary">
+      <div className="mx-2 md:mx-10 xl:mx-30 2xl:40 py-20">
+        <div className="columns-1 md:columns-3 gap-5 ">
+          <div className="mb-5 grid md:grid-cols-[30%70%] aspect-auto justify-items-start ">
+            <div className="color-primary flex md:flex-col gap-5 md:gap-15 justify-center ">
+              <p className="md:rotate-90 outfit xl:font-semibold ">My Work</p>
+              <p className="border rotate-90"></p>
+            </div>
+
+            <div className="color-primary flex flex-col gap-5 ">
+              <p className="font-bold outfit text-2xl md:text-3xl">
+                See My Works Which Will Amaze You!
+              </p>
+              <p className="poppins text-justify">
+                I develop simple, intuitive and responsive user interface that
+                helps users get things done with less effort and time with those
+                technologies.
+              </p>
+            </div>
+          </div>
+
+          {myWorks.map((data, index) =>
+            (index + 1) % 2 == 0 ? (
+              index + 1 == 6 || index + 1 == 8 ? (
+                <img
+                  onClick={() =>
+                    navigate(
+                      `/portfolio/${data.name
+                        .split(" ")
+                        .join("-")
+                        .toLowerCase()}`
+                    )
+                  }
+                  className="aspect-auto object-cover mb-5 rounded cursor-pointer"
+                  key={index}
+                  src={data.image}
+                  alt=""
+                />
+              ) : (
+                <img
+                  onClick={() =>
+                    navigate(
+                      `/portfolio/${data.name
+                        .split(" ")
+                        .join("-")
+                        .toLowerCase()}`
+                    )
+                  }
+                  className="aspect-auto object-cover mb-5 rounded cursor-pointer"
+                  key={index}
+                  src={data.image}
+                  alt=""
+                />
+              )
+            ) : index + 1 == 3 || index + 1 == 7 ? (
+              <img
+                onClick={() =>
+                  navigate(
+                    `/portfolio/${data.name.split(" ").join("-").toLowerCase()}`
+                  )
+                }
+                className="aspect-auto object-cover mb-5 rounded cursor-pointer "
+                src={data.image}
+                alt=""
+              />
+            ) : (
+              <img
+                onClick={() =>
+                  navigate(
+                    `/portfolio/${data.name.split(" ").join("-").toLowerCase()}`
+                  )
+                }
+                className="aspect-auto object-cover mb-5 rounded cursor-pointer"
+                src={data.image}
+                alt=""
+              />
+            )
+          )}
+
+          <div className="aspect-auto text-center ">
+            <NavLink className="text-indigo-500 border-b border-indigo-500">
+              See Work
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio
